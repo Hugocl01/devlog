bcf6abd8d379b4e4def7bb1412fc26445a4a862c
hugocl01 <hugocayon@gmail.com>
2025-11-13 21:34:38 +0100

    Corrección de configuración y dependencias

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,8 +10,8 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-      plugins: [tailwindcss()],
-    },
+    plugins: [tailwindcss()],
+  },
 
   integrations: [react(), sitemap()],
 });
